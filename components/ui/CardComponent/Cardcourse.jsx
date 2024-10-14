3ac09e8b7a47506ca231e29a2c42a468d35ce9d9
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-10-14 15:23:47 +0530

    deployment error solved issue

--- components/ui/CardComponent/Cardcourse.jsx
+++ components/ui/CardComponent/Cardcourse.jsx
@@ -195,7 +195,7 @@ const CardComponent = () => {
 
         {/* View All Button */}
         <div className="flex justify-center mt-4">
-          <a target="_blank"
+          <a 
             href={viewAllLinks[activeSection].link}
             className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition"
           >
