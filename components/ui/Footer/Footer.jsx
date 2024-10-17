d6f7c0f4bb4496ad10f1f7884ad9841a037940ca
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-10-17 18:54:40 +0530

    Update Footer.jsx

--- components/ui/Footer/Footer.jsx
+++ components/ui/Footer/Footer.jsx
@@ -107,8 +107,9 @@ const Footer = () => {
                             <input type="email" name="email" id="email" placeholder="Enter your email" class="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                         </div>
     
-                        <button type="submit" class="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Subscribe</button>
-                    </form>
+                        <button className="mt-4 py-2 px-4 text-white bg-red-500 rounded-md self-start">
+              Learn More
+            </button>                    </form>
                 </div>
             </div>
     
