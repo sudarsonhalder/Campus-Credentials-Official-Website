159bd1a5e7b457f7e56bc1bf754f34a20d89a02e
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-10 18:11:10 +0530

    push

--- components/ui/Brand/Brand.jsx
+++ components/ui/Brand/Brand.jsx
@@ -2,12 +2,13 @@ import Image from "next/image"
 
 const Brand = ({ ...props }) => (
     <Image
-        src="/CAMPUS.png"
+        src="https://ik.imagekit.io/99djpd8k3/CC/Campus%20Svg%20Logo.svg?updatedAt=1731242374124"
         alt="Campus Credentials logo"
         {...props}
         width={180}
         height={80}
         priority
+        unoptimized
     />
 )
 export default Brand
