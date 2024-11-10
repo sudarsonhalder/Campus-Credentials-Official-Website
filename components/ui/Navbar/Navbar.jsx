f3fe5148eb9f3bf35a699a6ccca2b4895754994b
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-10 17:51:28 +0530

    Update Navbar.jsx

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [isVisible, setIsVisible] = useState(true);
 
     const closeBanner = () => {
-      setIsVisible(false);
+        setIsVisible(false);
     };
     const [state, setState] = useState(false)
     const menuBtnEl = useRef()
@@ -16,8 +16,6 @@ const Navbar = () => {
         { name: "Courses", href: "/#Courses" },
         { name: "About Us", href: "/about-us" },
         { name: "Test Series", href: "/#Courses" },
-        // { name: "Testimonials", href: "/#testimonials" },
-        // { name: "FAQs", href: "/#faqs" },
         { name: "Free Resources", href: "https://resource.prepcrazy.com" },
     ]
 
@@ -29,43 +27,38 @@ const Navbar = () => {
     }, [])
 
     return (
-        <header className='relative'>
-         {isVisible && (
-       <div className="bg-amber-300 py-2 px-2 relative flex items-center justify-center">
-       {/* Centered Content */}
-       <div className="flex flex-col sm:flex-row items-center justify-center space-y-2 sm:space-y-0 sm:space-x-4">
-         {/* SVG Image */}
-         <img
-           src="https://ik.imagekit.io/99djpd8k3/CC/referal.4822d099%20(1).svg?updatedAt=1731239516352"
-           alt="Referral Icon"
-           className="w-4 h-4 sm:w-10 sm:h-10 object-contain hidden md:block"
-         />
-         <p className="text-base sm:text-md font-bold text-black">
-           Get a CC course with a Scholarship of 100%
-         </p>
-         <a
-           href="#"
-           className="px-2 py-1 text-white bg-red-500 rounded-md text-sm font-semibold hover:bg-red-500"
-         >
-           APPLY NOW
-         </a>
-       </div>
-     
-       {/* Close Button */}
-       <button
-         onClick={closeBanner}
-         className="absolute top-4 right-4 text-black focus:outline-none"
-         aria-label="Close banner"
-       >
-         ✕
-       </button>
-     </div>
-     
-      )}
+        <header className="relative shadow-lg">
+            {isVisible && (
+                <div className="bg-amber-300 py-2 px-2 relative flex items-center justify-center">
+                    <div className="flex flex-col sm:flex-row items-center justify-center space-y-2 sm:space-y-0 sm:space-x-4">
+                        <img
+                            src="https://ik.imagekit.io/99djpd8k3/CC/referal.4822d099%20(1).svg?updatedAt=1731239516352"
+                            alt="Referral Icon"
+                            className="w-4 h-4 sm:w-10 sm:h-10 object-contain hidden md:block"
+                        />
+                        <p className="text-base sm:text-md font-bold text-black">
+                            Get a CC course with a Scholarship of 100%
+                        </p>
+                        <a
+                            href="#"
+                            className="px-2 py-1 text-white bg-red-500 rounded-md text-sm font-semibold hover:bg-red-500"
+                        >
+                            APPLY NOW
+                        </a>
+                    </div>
+                    <button
+                        onClick={closeBanner}
+                        className="absolute top-4 right-4 text-black focus:outline-none"
+                        aria-label="Close banner"
+                    >
+                        ✕
+                    </button>
+                </div>
+            )}
             <div className="custom-screen md:hidden">
                 <NavHeader menuBtnEl={menuBtnEl} state={state} onClick={() => setState(!state)} />
             </div>
-            <nav className={`md:text-sm md:static md:block ${state ? "bg-gray-100 absolute z-20 top-0 inset-x-0 rounded-b-2xl shadow-xl md:bg-gray-900" : "hidden"}`}>
+            <nav className={`md:text-sm md:static md:block ${state ? "bg-gray-100 absolute z-20 top-0 inset-x-0 rounded-b-2xl shadow-xl md:bg-gray-900" : "hidden"} shadow-lg`}>
                 <div className="custom-screen items-center md:flex">
                     <NavHeader state={state} onClick={() => setState(!state)} />
                     <div className={`flex-1 items-center mt-8 text-gray-800 md:font-medium md:mt-0 md:flex ${state ? 'block' : 'hidden'} `}>
@@ -84,9 +77,10 @@ const Navbar = () => {
                         </ul>
                         <div className="gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
                             <Link href="/contact" className="block hover:text-gray-800 text-base font-semibold">
-Contact Us                            </Link>
+                                Contact Us
+                            </Link>
                             <NavLink href="" className="flex items-center justify-center gap-x-1 text-base text-white font-medium bg-red-500 active:bg-gray-900 md:inline-flex">
-                         Schedule a Call
+                                Schedule a Call
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                                 </svg>
