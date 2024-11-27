f97e0f28403dd04e80ab6003625e1ea7af5d1e53
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-27 23:40:37 +0530

    update yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Disables image optimization for static export
+  },
 }
 
 module.exports = nextConfig
