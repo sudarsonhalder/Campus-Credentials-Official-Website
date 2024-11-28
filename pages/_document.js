c3a12eb814f2540be0dcb0fbc621d7329c12402e
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-28 20:19:58 +0530

    removed npm nex export and g next

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <Script
-        src="https://embed.cal.com/embed.js"
-        strategy="beforeInteractive"
-      />
-      <Script async src="https://tally.so/widgets/embed.js"></Script>
       <body className='bg-white'>
         <Main />
         <NextScript />
