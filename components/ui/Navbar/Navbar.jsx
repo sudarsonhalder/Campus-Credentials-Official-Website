62c5363248b19c5e3b760332795adcac81719ea8
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-28 15:10:25 +0530

    Update Navbar.jsx

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -84,8 +84,8 @@ const Navbar = () => {
                             <Link href="/contact" className="block hover:text-gray-800 text-base font-semibold">
                                 Contact Us
                             </Link>
-                            <NavLink href="" className="flex items-center justify-center gap-x-1 text-base text-white font-medium bg-red-500 active:bg-gray-900 md:inline-flex">
-                          <span style={{textcolor:"white"}}>📞</span>   Schedule a Call
+                            <NavLink href="https://cal.com/campuscredentials" className="flex items-center justify-center gap-x-1 text-base text-white font-medium bg-red-500 active:bg-gray-900 md:inline-flex">
+                          <span style={{textcolor:"white"}}>📞</span>   Schedule a Meeting
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
                                     <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                                 </svg>
