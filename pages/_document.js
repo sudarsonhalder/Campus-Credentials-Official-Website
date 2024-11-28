a998a1880b53e4894b646169ee08faed5e47fde9
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-28 16:53:06 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <script
+      <Script
         src="https://embed.cal.com/embed.js"
         strategy="beforeInteractive"
       />
-      <script async src="https://tally.so/widgets/embed.js"></script>
+      <Script async src="https://tally.so/widgets/embed.js"></Script>
       <body className='bg-white'>
         <Main />
         <NextScript />
