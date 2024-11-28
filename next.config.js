470e94f762ccf3e3ca84a76c025a55ca85271b90
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-28 18:43:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Enables static export
-  // images: {
-  //   unoptimized: true, // Disables image optimization for static export
-  // },
+  images: {
+    unoptimized: true, // Disables image optimization for static export
+  },
 }
 
 module.exports = nextConfig
