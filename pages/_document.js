e5096ae6c3ea6a6c8ac7bedbcf90e1e9964790fa
Sudarson Halder <sudarsonhalder31@gmail.com>
2024-11-28 16:30:10 +0530

    Cal.com and Tally form Integrated

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
+      <script
+        src="https://embed.cal.com/embed.js"
+        strategy="beforeInteractive"
+      />
+      <script async src="https://tally.so/widgets/embed.js"></script>
       <body className='bg-white'>
         <Main />
         <NextScript />
