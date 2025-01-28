b02c14fdb040f4a6ecdfecbd3ae9cdc1891e276b
Paras Teli <parasteli2004@gmail.com>
2025-01-28 22:28:07 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Enables static export
+  output: "export", // Enables static export
   images: {
     unoptimized: true, // Disables image optimization for static export
   },
-  trailingSlash: true, // Adds trailing slash to all paths
-  assetPrefix: './', // Ensures assets load correctly in static exports
 };
 
 module.exports = nextConfig;
