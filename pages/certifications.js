6e941df11000118e075d7d89bab21e4671d03fb5
Sudarson Halder <sudarsonhalder31@gmail.com>
2025-02-10 19:01:24 +0530

    certifications course

---
+++ pages/certifications.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import CardCertificate from "../components/ui/CardCertificate/Cardcertificate"
+function certifications() {
+  return (
+   <CardCertificate/>
+  )
+}
+
+export default certifications
